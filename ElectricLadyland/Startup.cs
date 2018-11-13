19b77496fd6217bb5f0522d22111fd41319ec0d7
stevek <steve281kyles@gmail.com>
2018-11-12 17:28:18 -0800

    Temp error page fix

--- ElectricLadyland/Startup.cs
+++ ElectricLadyland/Startup.cs
@@ -27,7 +27,7 @@ namespace ElectricLadyland
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (true)//if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
